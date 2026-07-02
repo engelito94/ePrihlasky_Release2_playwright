a59b0bf08aa9b071e3fff88247629ab4111e348f
engelito94 <barcik@ditec.sk>
2026-07-02 15:38:03 +0200

    added new TC for "ZŠ"

---
+++ pages/prihlaska_ZS_page.py
@@ -0,0 +1,61 @@
+from playwright.sync_api import Page
+from pytest_playwright.pytest_playwright import page
+
+class PrihlaskaZS:
+    def __init__(self, page: Page):
+        self.page = page
+    
+    def pridanie_prihlasky(self):
+        self.page.wait_for_load_state("networkidle")
+        self.page.get_by_text("Vytvoriť prihlášku").first.click()
+        self.page.get_by_role("radio", name="Základná škola Prihlášku môž").check()
+        self.page.get_by_role("button", name="Pridať", exact=True).click()
+
+    def step_1_pridat_dieta(self, meno: str, priezvisko: str, rodne_cislo: str):
+        self.page.get_by_role("radio", name="Iné dieťa Pridajte dieťa").check()
+        self.page.get_by_role("button", name="Pridať dieťa").click()
+        self.page.get_by_role("radio", name="Áno").check()
+        self.page.get_by_role("textbox", name="Rodné číslo *").fill(rodne_cislo)
+        self.page.get_by_role("textbox", name="Krstné meno *").fill(meno)
+        self.page.get_by_role("textbox", name="Priezvisko *").fill(priezvisko)
+        self.page.locator("#step-1").get_by_role("button", name="Ďalej").click()
+        self.page.locator("#input-miestoNarodenia").fill("Slovensko")
+        self.page.locator("#adresaTPKrajina").get_by_role("textbox").fill("slovenska re")
+        self.page.locator("#adresaTPKrajina").get_by_text("Slovenská republika", exact=True).click()
+        self.page.locator("#adresaTPObec > .govuk-form-group > .input-wrapper > .govuk-input.autocomplete-input").fill("Brusno")
+        self.page.get_by_text("Brusno (Banská Bystrica)").click()
+        self.page.get_by_role("textbox", name="Krajina *").fill("cibulk")
+        self.page.get_by_text("Cibulková").click()
+        self.page.get_by_role("textbox", name="Súpisné číslo").fill("8")
+        self.page.get_by_role("textbox", name="Orientačné číslo *").fill("63")
+        self.page.get_by_role("textbox", name="PSČ *").fill("03687")
+        self.page.locator("#step-2").get_by_role("button", name="Pridať dieťa").click()
+        self.page.get_by_role("button", name="Ďalej").click()
+        self.page.get_by_role("button", name="Ďalej").click()
+
+    def step_2_SVVP(self):
+        self.page.get_by_role("radio", name="Náboženská").check()
+        self.page.get_by_role("radio", name="Rímskokatolícka").check()
+        self.page.locator("#zsDPDStravovanieRadio_option_0").check()
+        self.page.locator("#zsDPDSkolskyKlubRadio_option_0").check()
+        self.page.locator("#DPDSVVPRadio_option_1").check()
+        self.page.locator("#DPDDietaSNadanimRadio_option_1").check()
+        self.page.get_by_role("textbox", name="Poznámka:").fill("ŠVVP")
+        self.page.get_by_role("button", name="Ďalej").click()
+
+    def step_3_vyber_skoly(self, nazov_skoly: str):
+        self.page.get_by_role("radio", name="Hľadať podľa názvu školy").check()
+        self.page.get_by_role("textbox", name="Názov školy alebo jej adresa *").fill(nazov_skoly)
+        self.page.get_by_role("button", name="Hľadať").click()
+        self.page.get_by_role("button", name="Základná škola pre AT Pridať do prihlášky").click()
+        self.page.get_by_role("button", name="Ďalej").click()
+        self.page.get_by_role("button", name="Ďalej").click()
+        self.page.get_by_role("button", name="Ďalej").click()
+        self.page.get_by_role("button", name="Mám skontrolované").click()
+
+    def step_4_ZZ(self):
+        self.page.get_by_role("radio", name="Druhý zákonný zástupca nie je").check()
+        self.page.get_by_role("button", name="Ďalej").click()
+    
+    def step_5_prilohy(self):
+        self.page.get_by_role("button", name="Ďalej").click()
---
+++ pages/prilohy_ZS_page.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import Page
+from pytest_playwright.pytest_playwright import page
+
+class PrilohyZS:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def vyziadat_prilohu(self, dovod: str):
+        self.page.get_by_role("button", name="Vyžiadať prílohu").click()
+        self.page.get_by_label("Typ prílohy").select_option("3")
+        self.page.get_by_role("textbox", name="Dôvod: *").fill(dovod)
+        self.page.get_by_role("button", name="Odoslať").click()
+
+    def odvolat_ziadost(self):
+        self.page.get_by_role("button", name="Odvolať žiadosť").click()
+        self.page.get_by_role("button", name="Áno, odvolať").click()
+        self.page.wait_for_timeout(2000)
+
+    def pridat_prilohu(self):
+        self.page.get_by_role("link", name="Pridať prílohy").click()
+        self.page.get_by_text("Čestné vyhlásenie zákonného zástupcu Nenahrané Nenahrané Nahrané").click()
+        
+        with self.page.expect_file_chooser() as fc_info:
+            self.page.get_by_role("link", name="Vybrať súbor").click()
+
+        file_chooser = fc_info.value
+        file_chooser.set_files("./data/Dokument.pdf")
+        self.page.get_by_role("button", name="Odoslať").click()
+
+    def click_on_prejst_na_prihlasky(self):
+        self.page.get_by_role("link", name="Prejsť na prihlášky").click()
